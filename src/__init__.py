685364e6aa296e3bfa979a9240a345d8b943327a
tiksharsh <tiksharsh@icloud.com>
2023-03-10 22:54:23 +0530

    EDA & Model Training

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+
+# Enviornoment creation command
+# conda create -p venv python==3.8 -y
+
+# Created enviornoment (venv) activate command with complete path
+#  conda activate /Users/Wolverine/Documents/GitHub/KrishNaik/End-To-End-ML-Project-With-Deployment/mlproject/venv
